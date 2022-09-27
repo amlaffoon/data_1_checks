1292e7c16bb9cc4ce9433b4c1b7902619efe42de
Anna Laffoon <ree11194@gmail.com>
2022-09-27 14:29:59 -0400

    init commit

---
+++ check_one.py
@@ -0,0 +1,26 @@
+# hello world statement
+print("hello world")
+
+# print list value
+my_list = [
+    "lepidoptera saturniidae", 
+    "lepidoptera nymphalidae", 
+    "odonata libellulidae", 
+    "odonata coenagrionidae"
+    ]
+
+print(my_list[2])
+
+# print dictionary value
+my_dict = {
+"lepidoptera": "moths and butterflies",
+"odonata": "damselflies and dragonflies"
+}
+values = list(my_dict.values()) 
+
+print(values[0])
+
+# print tuple value
+my_tuple = ("moth", "butterfly", "dragonfly", "damselfly")
+
+print(my_tuple[2])
